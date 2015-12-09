fc50739aa85f513cb0b72561023a4e6f218246b2
Edward Kim <edward@webconn.me>
2015-12-10 07:43:07 +0900

    Used go-webconn package

--- app/src/app_relay/app_relay.go
+++ app/src/app_relay/app_relay.go
@@ -2,6 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2015 Jane Lee <jane@webconn.me>
+ * Copyright (c) 2015 Edward Kim <edward@webconn.me>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,20 +26,16 @@
 package main
 
 import (
+    "github.com/webconnme/go-webconn"
+)
+
+import (
 	"fmt"
 	"log"
-	"net/http"
-	"io/ioutil"
-	"encoding/json"
 	"ioctl"
 	"unsafe"
 	"os"
 )
-
-type jconfig struct {
-	Command	string `json:"command"`
-	Data	string `json:"data"`
-}
 
 type GpioS struct {
 	io int
@@ -51,13 +48,13 @@ const (
 	IOCTL_GPIO_GET_OUTPUT = 7239682
 )
 
-var gpioPath string
+var client webconn.Webconn
+
 var file *os.File
-var url string
 
 func GPIOOpen() {
 	var err error
-	file, err = os.OpenFile(gpioPath, os.O_RDWR | os.O_SYNC, 0777)
+	file, err = os.OpenFile("/dev/ioctl_gpio", os.O_RDWR | os.O_SYNC, 0777)
 	if err != nil {
 		log.Fatal("open", err)
 	}
@@ -70,69 +67,31 @@ func GPIOClose() {
 	fmt.Println("GPIO Close...")
 }
 
-func httpGet(ch chan<- int) {
-
-	var jconf []jconfig
-
-	for {
-		client := &http.Client{}
-
-		resp, err := client.Get(url)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		contexts, err := ioutil.ReadAll(resp.Body)
-
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		fmt.Println(">>>contexts : ",string(contexts), " for the url : ", url)
-		json.Unmarshal(contexts,&jconf)
-		for _, j:=range jconf {
-			if j.Command == "power" {
-				if j.Data == "on" {
-					ch <- 1 //high
-				}else if j.Data == "off" {
-					ch <- 0    //low
-				}
-			}
-		}
-	}
-}
-
-func GpioOut(ch <-chan int) {
-
-	g := GpioS {0, 0, 0}
+func GpioOut(buf []byte) error {
+	g := GpioS {28, 0, 0}
 	header := unsafe.Pointer(&g)
-	g.io = 28
 
-	for {
+    state := string(buf)
 
-		g.value = <-ch
-		ioctl.IOCTL(uintptr(file.Fd()), IOCTL_GPIO_SET_OUTPUT, uintptr(header))
+    if state == "on" {
+        g.value = 1
+    }else if state == "off" {
+        g.value = 0
+    }
 
-		fmt.Println(">>>power stat : ", g.value)
-	}
+    ioctl.IOCTL(uintptr(file.Fd()), IOCTL_GPIO_SET_OUTPUT, uintptr(header))
+
+    fmt.Println(">>>power stat : ", g.value)
+
+    return nil
 }
 
 func main() {
-
-	channel := make(chan bool)
-	txchan := make(chan int)
-
-	url = "Http://nor.kr:3002/v01/relay/80"
-	gpioPath = "/dev/ioctl_gpio"
-
 	GPIOOpen()
+    defer GPIOClose()
 
-	go httpGet(txchan)
-	go GpioOut(txchan)
+    client = webconn.NewClient("http://nor.kr:3002/v01/relay/80")
+    client.AddHandler("power", GpioOut)
 
-	<-channel
-
-	GPIOClose()
+    client.Run()
 }
